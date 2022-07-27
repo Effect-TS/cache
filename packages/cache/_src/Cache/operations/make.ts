d3cf104651cfdd27322dca0ecf30500caaf166ba
Maxwell Brown <maxwellbrown1990@gmail.com>
2022-07-27 14:36:36 +0000

    feat(@effect/cache): upgrade Effect to v0.0.8

--- packages/cache/_src/Cache/operations/make.ts
+++ packages/cache/_src/Cache/operations/make.ts
@@ -9,6 +9,6 @@ export function make<Key, Environment, Error, Value>(
   timeToLive: Duration,
   lookup: Lookup<Key, Environment, Error, Value>,
   __tsplusTrace?: string
-): Effect.RIO<Environment, Cache<Key, Error, Value>> {
+): Effect<Environment, never, Cache<Key, Error, Value>> {
   return Cache.makeWith(capacity, lookup, () => timeToLive)
 }
